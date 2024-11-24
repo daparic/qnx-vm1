c8f3ca275cd1e355ce2d21357301b3dcc86ffcc7
dx <dx@nuc>
2024-11-24 23:33:57 +0800

    init

---
+++ boot.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+
+[ ! -e disk-qemu ] && cat chunk_* > disk-qemu
+qemu-system-x86_64 \
+    -smp 2 \
+    --enable-kvm \
+    --cpu host \
+    -m 1G \
+    -drive file=disk-qemu.vmdk,if=ide,id=drv0 \
+    -netdev bridge,br=virbr0,id=net0 \
+    -device virtio-net-pci,netdev=net0,mac=52:54:00:cb:95:f2 \
+    -pidfile qemu.pid \
+    -nographic \
+    -kernel ifs.bin \
+    -serial mon:stdio \
+    -object rng-random,filename=/dev/urandom,id=rng0 \
+    -device virtio-rng-pci,rng=rng0 \
+    -nographic \
+    -no-reboot
